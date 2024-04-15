0a91bcd75d8ddad8bb736753c5b4789b65baca7c
PinyPon <tizianaperfumobouzon@gmai.com>
2024-04-15 10:56:43 -0300

    subo cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './secciones/inicio/inicio.component';
+import { GaleriaComponent } from './secciones/galeria/galeria.component';
+import { RootNavComponent } from './root-nav/root-nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  {path:'menu', component:RootNavComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
