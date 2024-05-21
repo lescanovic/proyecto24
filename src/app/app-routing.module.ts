02b3ec5a4040b62057e66cd2374f6f7cd7366c7b
lescanovic <victorialescanoarco@gmail.com>
2024-05-21 10:57:20 -0300

    configuracion del modulo de autentificacion y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component'
 const routes: Routes = [
   {path:"",component: InicioComponent,},
   {path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)},
-  {path:"",loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule)}
+  {path:"",loadChildren:()=>import('./modules/producto/producto.module').then(m=>m.ProductoModule)},
+  {path:"",loadChildren:()=>import('./modules/autentificacion/autentificacion.module').then(m=>m.AutentificacionModule)}
 ];
 
 @NgModule({
