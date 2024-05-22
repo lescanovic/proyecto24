ade192bcdc0ca60ced4efd8325c507ad125ac399
lescanovic <victorialescanoarco@gmail.com>
2024-05-22 11:12:39 -0300

    Subo cambios nuevos

--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
- 
+//componentes de material 
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatButtonModule} from '@angular/material/button';
+
 //Vistas de autentificacion
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
 import { RegistroComponent } from './pages/registro/registro.component';
@@ -15,11 +20,19 @@ import { IniciosesionComponent } from './pages/iniciosesion/iniciosesion.compone
   imports: [
     CommonModule,
     AutentificacionRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
 
   ],
   exports: [
     RegistroComponent,
-    IniciosesionComponent
+    IniciosesionComponent,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
 })
 export class AutentificacionModule { }
