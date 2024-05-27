917369291a78d80387cbee9e60766984724fcf99
lescanovic <victorialescanoarco@gmail.com>
2024-05-27 10:52:11 -0300

    subo cambios

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+    //lo mismo que un identificador pero este se vincula con la base de datos
+    uid: string | any; //recibe valores vacios o indefinidos
+    nombre: string;
+    apellido: string;
+    email:string;
+    password: string;
+}
