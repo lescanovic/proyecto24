f2975f645a2c16e63e306ceff13c3e180f781fb2
lescanovic <victorialescanoarco@gmail.com>
2024-07-01 11:04:36 -0300

    encriptacion de contraseña de usuario haciendo uso de Crypto Js

--- src/app/modules/autentificacion/pages/registro/registro.component.ts
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth.service';
 //importamos componente de ruta de angular
 import { Router } from '@angular/router';
 import { FirestoreService } from 'src/app/modules/shared/service/firestore.service';
-
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-registro',
@@ -35,60 +35,60 @@ export class RegistroComponent {
     public servicioRutas: Router
 
   ) { }
-  async guardarUsuario(){
-this.servicioFirestore.agregarUsuario(this.usuarios, this.usuarios.uid)
-.then(res =>{
-  console.log(this.usuarios);
+   async guardarUsuario() {
+   this.servicioFirestore.agregarUsuario(this.usuarios, this.usuarios.uid)
+      .then(res => {
+       console.log(this.usuarios);
 
-})
-.catch(erro =>{
-  console.log('Error =>',erro)
-})
-}
+      })
+      .catch(erro => {
+        console.log('Error =>', erro)
+       })
+      this.usuarios.password = c
+  }
   async registrar() {
-    /*
-    const credenciales = {
-      uid: this.usuarios.uid,
-      nombre: this.usuarios.nombre,
-      apellido: this.usuarios.apellido,
-      email: this.usuarios.email,
-      rol: this.usuarios.rol,
-      password: this.usuarios.password
-    }
-*/
+    
     const credenciales = {
       email: this.usuarios.email,
       password: this.usuarios.password
     }
     const res = await this.servicioAuth.registrar(credenciales.email, credenciales.password)
-    //promesa
-    .then(res =>{
-      alert("Se pudo registrar con exito")
-      this.servicioRutas.navigate(['/inicio'])
-    })
-//el metodo catch captura una falla y la vuelve un error cuando la promesa salga mal
-    .catch(error =>{
-      alert("Hubo un problema al registrar un nuevo usuario :c \n"+error)
-    })
-//constante uid captura el identificador de la BD
-const uid = await this.servicioAuth.obtenerUid();
-//se le asigna al atributo de la interfaz esta constante
-this.usuarios.uid = uid ;
-//llamamos a la funcion guardarUsuario
-this.guardarUsuario()
+      //promesa
+      .then(res => {
+        alert("Se pudo registrar con exito")
+        this.servicioRutas.navigate(['/inicio'])
+      })
+      //el metodo catch captura una falla y la vuelve un error cuando la promesa salga mal
+      .catch(error => {
+        alert("Hubo un problema al registrar un nuevo usuario :c \n" + error)
+      })
+    //constante uid captura el identificador de la BD
+    const uid = await this.servicioAuth.obtenerUid();
+    //se le asigna al atributo de la interfaz esta constante
+    this.usuarios.uid = uid;
+    //llamamos a la funcion guardarUsuario
+    this.guardarUsuario()
 
 
-this,limpiarInputs();
+   
+
+this.limpiarInputs()
+ }
 
 
 
-    //Registro con servicio
-    // this.coleccionUsuarios.push(credenciales)
 
 
 
+  limpiarInputs(){
+    const inputs = {
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      email: this.usuarios.email = '',
+      rol: this.usuarios.rol = '',
+      password: this.usuarios.password = ''
+    }
   }
-
-
 
 }
