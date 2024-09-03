8d17f6c13e59514193007bcce2c499f52a250dbf
lescanovic <victorialescanoarco@gmail.com>
2024-09-03 11:04:02 -0300

    Creamos las directivas Input y Output, Conectamos las informacion transmitida entre el componente padre e hijo, mostramos la informacion en un carrusel y una notificacion, #El codigo tiene errores#

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+import { Input,Output,EventEmitter } from '@angular/core';
 //IMPORTAMOS INTERFAZ
-import { Personaje } from 'src/app/models/personaje';
+import { CrudService } from 'src/app/modules/admin/service/crud.service';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-card',
@@ -11,33 +12,46 @@ import { Personaje } from 'src/app/models/personaje';
 
 })
 export class CardComponent {
- //PROPIEDAD PUBLICA TIPO ARRAY
- public info: Personaje [] ;
- constructor(){
-  this.info = [ 
-{
-  id: "",
-  nombre: "Harry Potter",
-  edad: 11,
-  imagen: "https://hips.hearstapps.com/hmg-prod/images/in-arrivo-reboot-film-harry-potter-nuovi-attori-1672747363.png?crop=0.611xw:1.00xh;0.111xw,0&resize=640:*",
-  alt: 'Harry '
-},
-{
-  id: '',
-  nombre: 'Ron Weasly',
-  edad: 11,
-  imagen: 'https://caracoltv.brightspotcdn.com/dims4/default/c37eec7/2147483647/strip/false/crop/700x484+0+0/resize/1200x829!/quality/90/?url=https%3A%2F%2Fcaracol-brightspot.s3-us-west-2.amazonaws.com%2Fassets%2Fshock%2Fcontent_files%2F2015_08%2Fgallery%2Fronw0.jpg',
-  alt: 'Ron'
-},
-{
-  id: '',
-  nombre: 'Hermione Granger',
-  edad:11,
-  imagen: 'https://i.pinimg.com/736x/66/37/00/663700f6077c91f7076157572d8e2d3e.jpg',
-  alt: 'Hermione'
+
+  coleccionProducto: Producto [] = []
+ 
+  productoSeleccionado!: Producto ;
+
+  modalVisible : boolean = false
+
+  compraVisible: boolean = false
+//directiva para comunicarse con el componente padre
+  @Input() productoReciente: string = '';
+
+  @Output() productoAgregado = new EventEmitter <Producto>()
+
+constructor(public servicioCrud:CrudService){}
+
+  ngOnInit() : void{
+this.servicioCrud.obtenerProducto().subscribe(producto=>{
+    this.coleccionProducto = producto
+})
+  }
+
+mostrarVer(info: Producto){
+  //cambio el estado del mdal
+this.modalVisible = true 
+
+this.productoSeleccionado = info
+
+}
+
+agregarProducto(info:Producto){
+  this.productoAgregado.emit(info)
+}
+
+ //enviando la informacion del producto que agregamos
+
+
+
 }
 
 
-] 
- }
-}
+
+
+
--- src/app/modules/producto/pages/producto/producto.component.ts
+++ src/app/modules/producto/pages/producto/producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-producto',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class ProductoComponent {
 
+  product: string = ''
+//coleccion que va a recibir los productos que se  vayan agregando mediante el Output
+  productosCarrusel : Producto[] = [] 
+
+  productoAnadido(producto: Producto){ 
+      this.product = `${producto.nombre} : $${producto.precio}`;
+    
+      try{
+        
+      /*Agregamos la informacion recibida por el parametro de la funcion 
+      a la coleccion del carusel
+      */
+     this.productosCarrusel.push(producto)
+
+     Swal.fire({
+      title:"¡Enhorabuena!",
+      text: "Ha añadido el producto con exito",
+      icon: 'info'
+     })
+      }
+      catch(error){
+        Swal.fire({
+          title:"¡Oh No!",
+          text: "Ha ocurrido un error\n"+error,
+          icon:'error'
+         })
+      }
+
+      /*Agregamos la informacion recibida por el parametro de la funcion 
+      a la coleccion del carusel
+      */
+     this.productosCarrusel.push(producto)
+
+    
+  }
 }
